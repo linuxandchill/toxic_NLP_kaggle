0e7e27735dbed246efd7ec2d34b5b0714c42e9eb
linuxandchill <rivercodi@gmail.com>
2018-07-25 13:27:24 -0700

    load up embedding

--- toxic-classifier.py
+++ toxic-classifier.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import numpy as np
 import pandas as pd
@@ -9,3 +10,19 @@ from keras.layers import Conv1D, Embedding, GlobalMaxPooling1D
 from keras.models import Model
 from sklearn.metrics import roc_auc_score
 
+MAX_SEQUENCE_LENGTH = 100
+MAX_VOCAB_SIZE = 20000
+EMBEDDING_DIM = 100 ## gloVe comes in specific sizes
+VALIDATION_SPLIT = 0.2
+BATCH_SIZE = 128
+EPOCHS = 10
+
+word2vec = {}
+with open('../../../passport/toxic-comments/glove.6B/glove.6B.100d.txt') as file:
+    for line in file:
+        values = line.split()
+        word = values[0] # first item is word
+        vec  = np.asarray(values[1:], dtype='float32') #rest of line is emb
+        word2vec[word] = vec
+#print(len(word2vec)) #40k 
+print(word2vec['and'])
